2fa87be1c5079cf04ea3b587f2976fc6876166c4
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-08 20:31:32 +0300

    add loader functions to loader the news data

--- src/loader.py
+++ src/loader.py
@@ -1,8 +1,5 @@
-import json
-import argparse
+import pandas as pd
 import os
-
-
 
 # Create wrapper classes for using news_sdk
 class NewsDataLoader:
@@ -11,9 +8,29 @@ class NewsDataLoader:
 
 
     '''
-    def __init__(self, path, domains_path, traffic_path):
+    def __init__(self, news_data_path, domains_path, traffic_path):
         '''
         path: path to the news exported data folder
         '''
-        self.path = path
+        self.news_data = news_data_path
         self.domains_path = domains_path
+        self.traffic_path = traffic_path
+
+        self.news_data = self.get_news_data()
+        self.domains_data = self.get_domains_data()
+        self.traffic_data = self.get_traffic_data()
+
+    def load_news_data(self):
+        """Loads news articles from data.csv"""
+        news_df = pd.read_csv(os.path.join(self.news_data, "data.csv"))
+        return news_df
+
+    def load_domain_info(self):
+        """Loads info about news source domains"""
+        domains_df = pd.read_csv(os.path.join(self.domains_path, "domains_location.csv"))
+        return domains_df
+
+    def load_traffic_data(self):
+        """Loads website traffic data"""
+        traffic_df = pd.read_csv(os.path.join(self.traffic_path, "traffic_data.csv"))
+        return traffic_df
