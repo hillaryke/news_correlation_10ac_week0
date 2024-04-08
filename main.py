45b80f27d0419b2dc7cc40686ad734ca2d39328e
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-08 22:25:21 +0300

    Get the top countries with highest number of news media organizations (using domains in the data)

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from src.config import cfg  # Import the config file
 from src.loader import DataLoader
 from src.utils import largest_news_count_websites
 from src.utils import largest_number_of_traffic_websites
+from src.utils import get_countries_with_most_media_organizations
 
 data_path = cfg.news_data_path
 
 loader = DataLoader()
 traffic_data = loader.load_traffic_data()
 news_data = loader.load_news_data()
+domain_info = loader.load_domain_info()
 
 top_N = 10
 
@@ -17,4 +19,8 @@ print("Top 10 websites with the largest number of news articles:")
 
 # Display the top 10 websites with the largest number of visitors
 print("Top 10 websites with the largest number of visitors:")
-print(largest_number_of_traffic_websites(traffic_data, top_N))
+# print(largest_number_of_traffic_websites(traffic_data, top_N))
+
+# Display the top 10 countries with the highest number of news media organizations
+print("Top 10 countries with the highest number of news media organizations:")
+print(get_countries_with_most_media_organizations(domain_info, top_N))
--- src/utils.py
+++ src/utils.py
@@ -18,3 +18,12 @@ def largest_number_of_traffic_websites(traffic_data, top_N):
 
     # Return the top N website sources
     return sorted_traffic_data[['Domain', 'RefIPs']].head(top_N)
+
+def get_countries_with_most_media_organizations(domain_info, top_N):
+    """Get the top N countries with the highest number of news media organizations"""
+
+    # Use the `value_counts()` function on the 'location' column
+    country_counts = domain_info['location'].value_counts()
+
+    # Return the top N countries
+    return country_counts.head(top_N)
