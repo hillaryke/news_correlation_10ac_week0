da72df20bca4bce509ceb8e5bf402d65ef44b166
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-08 22:06:58 +0300

    run and display the largest news count website sources

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from src.config import cfg  # Import the config file
 from src.loader import NewsDataLoader
+from src.utils import largest_news_count_websites
 
 data_path = cfg.news_data_path
 
 loader = NewsDataLoader()
 news_data = loader.load_news_data()
 
-print(news_data.head())
+# Display the top 10 websites with the largest count of news articles
+top_N = 10
+print(largest_news_count_websites(news_data, top_N))
