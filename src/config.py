fc441757c3d3097c556b61f78a633960e7eb469d
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-08 21:51:44 +0300

    Place the path of the data files in variables and use config to load them

--- src/config.py
+++ src/config.py
@@ -1,10 +1,15 @@
 from __future__ import print_function
 import argparse
 
+news_data_path = 'data/'
+domains_path = 'data/'
+traffic_path = 'data/'
+
 parser = argparse.ArgumentParser(description='cmdArgs')
 parser.add_argument('--output', type=str, default='news_analysis.csv',
                 help='filename to write analysis output in CSV format')
-parser.add_argument('--news_data_path', required=True, type=str, help='directory to where main news dataset reside')
+parser.add_argument('--news_data_path', required=False, type=str,
+                    help='directory to where main news dataset reside')
 
 
 cfg = parser.parse_args()
--- src/loader.py
+++ src/loader.py
@@ -1,39 +1,31 @@
 import pandas as pd
 import os
 import argparse
+from src import config
+
 
 # Create wrapper classes for using news_sdk
 class NewsDataLoader:
     '''
     News exported data IO class.
-
-
     '''
-    def __init__(self, data_path, domains_path, traffic_path):
-        '''
-        path: path to the news exported data folder
-        '''
-        self.data_path = data_path_path
-        self.domains_path = domains_path
-        self.traffic_path = traffic_path
 
-        self.data_path = self.get_data_path()
-        self.domains_data = self.get_domains_data()
-        self.traffic_data = self.get_traffic_data()
+    def __init__(self):
+        pass
 
-    def load_new_data(self):
+    def load_news_data(self):
         """Loads news articles from data.csv"""
-        news_df = pd.read_csv(os.path.join(self.data_path, "data/data.csv"))
+        news_df = pd.read_csv(os.path.join(config.news_data_path, "data.csv"))
         return news_df
-
+#
     def load_domain_info(self):
         """Loads info about news source domains"""
-        domains_df = pd.read_csv(os.path.join(self.domains_path, "domains_location.csv"))
+        domains_df = pd.read_csv(os.path.join(config.domains_path, "domains_location.csv"))
         return domains_df
 
     def load_traffic_data(self):
         """Loads website traffic data"""
-        traffic_df = pd.read_csv(os.path.join(self.traffic_path, "traffic_data.csv"))
+        traffic_df = pd.read_csv(os.path.join(config.traffic_path, "traffic.csv"))
         return traffic_df
 
 if __name__ == '__main__':
