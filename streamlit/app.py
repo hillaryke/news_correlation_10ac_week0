4fe50962a4773263244a2363fcfa46168054976e
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-13 09:52:23 +0300

    add news analysis dashboard sidebar for streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -8,11 +8,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import altair as alt
 
-
-
-def load_data(file_path):
-    news_data = pd.read_csv(file_path)
-    return news_data
+alt.themes.enable("dark")
 
 ##################################
 # Page configuration
@@ -22,6 +18,78 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+
+#######################
+# CSS styling
+st.markdown("""
+<style>
+
+[data-testid="block-container"] {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    margin-bottom: -7rem;
+}
+
+[data-testid="stVerticalBlock"] {
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+
+[data-testid="stMetric"] {
+    background-color: #393939;
+    text-align: center;
+    padding: 15px 0;
+}
+
+[data-testid="stMetricLabel"] {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+[data-testid="stMetricDeltaIcon-Up"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+[data-testid="stMetricDeltaIcon-Down"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+#######################
+# Load data
+df_reshaped = pd.read_csv('../data/findings/tags_count.csv')
+
+#######################
+# Sidebar
+with st.sidebar:
+    st.title(':newspaper: News analysis Dashboard')
+
+
+    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo',
+                        'viridis']
+    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+
+
+#######################
+
+def load_data(file_path):
+    news_data = pd.read_csv(file_path)
+    return news_data
+
 
 alt.themes.enable("dark")
 
